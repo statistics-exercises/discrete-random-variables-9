e42eaf80604ccc2c2b8b8928cfc7e8d13953d231
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-26 19:03:21 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def bernoulli(p) : 
+  # Your code to generate a bernoulli random variable goes here
+  
+def geometric(p) : 
+  # Your code to generate a geometric random variables goes here
+  
+def negative_binomial(k,p) :
+  # Your code to generate a negative binomial random variable goes here
+  
+  
+  
+print( negative_binomial(3,0.5), negative_binomial(3,0.5), negative_binomial(3,0.5) )
